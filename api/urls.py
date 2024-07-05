7da10e6ce552283d6b9c6e2a6cb34322bbd465ad
Komiljnovmuhammadziyo <muhammadziyo056@gmail.com>
2024-07-05 15:17:32 +0500

    Django Rest framework, API lar bilan ishlash

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ from os import name
 from django.urls import path
 
 from api.views import BookReviewDetailAPIVIew
+app_name = 'api'
 
 urlpatterns = [
     path('review-detail/<int:id>/', BookReviewDetailAPIVIew.as_view(), name='review-detail'),
